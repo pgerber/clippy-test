7c3e6b3ea0d644a219f3bf46037acf065b61e961
Peter Gerber <peter@arbitrary.ch>
2017-04-04 00:16:06 +0200

    add test cases

---
+++ src/main.rs
@@ -0,0 +1,85 @@
+#![feature(plugin)]
+#![plugin(clippy)]
+
+use std::fs::{DirBuilder, OpenOptions};
+use std::os::unix::fs::{DirBuilderExt, OpenOptionsExt};
+
+fn main() {
+    DirBuilder::new()
+        .recursive(true)
+        .mode(0o700)
+        .create("/home/peter")
+        .unwrap();
+
+    DirBuilder::new()
+        .recursive(true)
+        .mode(0o777)
+        .create("/home/peter")
+        .unwrap();
+
+    #[warn(world_readable_file_mode)]
+    DirBuilder::new()
+        .recursive(true)
+        .mode(0o775)
+        .create("/home/peter")
+        .unwrap();
+
+    DirBuilder::new()
+        .recursive(true)
+        .mode(0o0)
+        .create("/home/peter")
+        .unwrap();
+
+    DirBuilder::new()
+        .recursive(true)
+        .mode(488) // 0o750
+        .create("/home/peter")
+        .unwrap();
+
+    OpenOptions::new().mode(0o777);
+
+    OpenOptions::new()
+        .mode(0o77777)
+        .open("abc")
+        .unwrap();
+
+    OpenOptions::new()
+        .mode(0o4750)
+        .open("abc")
+        .unwrap();
+
+    OpenOptions::new()
+        .mode(0o2750)
+        .open("abc")
+        .unwrap();
+
+    let mode = 0o2000;
+    OpenOptions::new()
+        .mode(mode)
+        .open("abc")
+        .unwrap();
+
+    const MODE: u32 = 0o4000;
+    OpenOptions::new()
+        .mode(MODE)
+        .open("abc")
+        .unwrap();
+
+    OpenOptions::new()
+        .mode(get_mode())
+        .open("abc")
+        .unwrap();
+
+    S.mode(0o007);
+}
+
+struct S;
+
+impl S {
+    fn mode(&mut self, _mode: u32) {}
+}
+
+#[inline(always)]
+fn get_mode() -> u32 {
+    0o3000
+}
